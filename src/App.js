9ce2dc6ad9898ebe5fd4671d9232a6e9ae98272d
andart71 <one.desiree@yandex.ru>
2021-02-11 17:18:09 +0600

    еу

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Button } from '@sberdevices/ui/components/Button/Button';
+
+function App() {
+    return (
+        <div className="App">
+            <p>
+                <Button view="primary">Hello Plasma</Button>
+            </p>
+        </div>
+    );
+}
+
+export default App;
